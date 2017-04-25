f97349742739c239421f233a24f322f0ef0951d7
joemcdonnell50 <joemcdonnell50@gmail.com>
2017-04-25 16:33:10 +0100

    Update Customer.java
    Added state for State design pattern

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Customer;
 
 /**
@@ -10,13 +5,24 @@ package Customer;
  * @author bprieto
  */
 public class Customer {
+    //Factory method for Customer
+    public static Customer create(String username, String password) {
+        return new Customer(username, password);
+    }
     private String username;
     private String password;
+    private State state;
 
     public Customer(String username, String password) {
         this.username = username;
         this.password = password;
     }
+    public void setState(State state){
+      this.state = state;		
+   }
+    public State getState(){
+      return state;
+   }
 
     public String getUsername() {
         return username;
